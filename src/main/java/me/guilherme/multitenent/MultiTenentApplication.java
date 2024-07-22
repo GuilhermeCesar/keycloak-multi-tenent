57c392a2f011a32a18853241eea12eddc25bd36c
guilhermecesar <guilherme.medeiros@platformbuilders.io>
2024-07-22 15:33:47 -0300

    first commit

---
+++ src/main/java/me/guilherme/multitenent/MultiTenentApplication.java
@@ -0,0 +1,13 @@
+package me.guilherme.multitenent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/guilherme/multitenent/MultiTenentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.guilherme.multitenent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
